cf05da19118957513426179084c7262e81d65ce4
Warren Kushner <warren.kushner@gmail.com>
2014-10-28 17:58:33 -0400

    should be all sqaured with migrations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ActiveRecord::Base
    # belongs_to :captain, class_name: "User"
     has_many :teammates, class_name: "User"
+    belongs_to :captain, class_name: "User"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026224457) do
+ActiveRecord::Schema.define(version: 20141028031540) do
 
   create_table "team_tables", force: true do |t|
     t.integer "captain_id"
   end
 
   create_table "teams", force: true do |t|
-    t.integer "captain_id"
     t.string  "name"
     t.string  "sport"
     t.string  "location"
-  end
-
-  create_table "teams_tables", force: true do |t|
     t.integer "captain_id"
   end
 
-# Could not dump table "users" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  add_index "teams", ["captain_id"], name: "index_teams_on_captain_id"
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+    t.string   "type"
+    t.string   "role"
+    t.integer  "team_id"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
