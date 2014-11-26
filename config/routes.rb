34b54cd4df6149d03f23d34a4e010fa54d4c011f
Warren <warren.kushner@gmail.com>
2014-11-26 17:03:56 -0500

    added buttons and generated team controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
 
+  
+
+  get 'teamcaptains/show'
+
  # devise_for :users, :controllers => {:registrations => "registrations"}
   devise_for :users
   get "/users/:id" => "users#binary_selection", as: 'binary_selection'
   put "/users/:id" => "users#binary_selection_update", as: 'binary_selection_update'
-
-  resources :users
-
+  
+  resources :teams do
+    resources :teammates
+  end
+  resources :teamcaptain do 
+    resources :teams
+  end
   #get "/users" => "users#update", as: 'update'
   
   get 'welcome/index'
