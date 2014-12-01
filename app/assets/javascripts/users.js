a8a241a64cbfa86dda6fdd70923c9e3caa87be95
Warren <warren.kushner@gmail.com>
2014-12-01 01:01:02 -0500

    added jquery to toggle text field values

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // put all your jQuery goodness in here.
+    $('#role_button_captain').click(function() {
+       $('#user_role_attribute').val('captain');
+    });
+
+    $('#role_button_teammate').click(function() {
+        $('#user_role_attribute').val('teammate');
+    });
+});
--- app/assets/javascripts/users.js.coffee
+++ app/assets/javascripts/users.js.coffee
@@ -2,3 +2,5 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 $.ajax url: "/users/binary_selection", format: 'js'
+
+alert('hello');
