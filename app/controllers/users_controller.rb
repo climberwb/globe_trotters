fa8c6fc69476a32eb774bd4413a595d20d84f0c4
Warren <warren.kushner@gmail.com>
2014-12-16 23:40:39 -0500

    added admin profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
   respond_to :json, :html
 
+  def admin_form
+    @user = current_user
+  end
+  def admin_info_create
+    @admin = current_user.update_attributes(
+              :bio => params[:user][:bio],
+              :avatar => params[:user][:avatar]
+            )
+    #if @admin.present?
+      #team1.teammates << @teammates
+      redirect_to admin_show_path(current_user)
+   # end
+  end
+
+  def admin_show
+    @admin = User.find(params[:id]) 
+  end
+
    def update
     if current_user.update_attributes(user_params)
       flash[:notice] = "User information updated"
@@ -19,15 +37,15 @@ class UsersController < ApplicationController
     url = @user.id
     if @user.update_attribute(:role, params[:user][:role]) 
       if @user.role == "captain"
-       # '/captains/'"#{url}"'/teams/new'
-      redirect_to new_captain_team_path(current_user.id), notice: "#{@user.id} was updated."
+         redirect_to new_captain_team_path(current_user.id), notice: "#{@user.id} was updated."
       elsif @user.role == "teammate"
-        redirect_to new_teammate_path
+          redirect_to new_teammate_path
       end
     else
      # format.html { render action: "edit" }
     end
   end
+
 
   def new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
 
   devise_for :users
+  
+  get "/users/:id" => "users#admin_show", as: 'admin_show'
+  get "/users/:id" => "users#admin_form", as: 'admin_form'
+  put "/users/:id" => "users#admin_info_create", as: 'admin_info_create'
+
+
   get "/users/:id" => "users#binary_selection", as: 'binary_selection'
   put "/users/:id" => "users#binary_selection_update", as: 'binary_selection_update'
 
@@ -22,7 +28,7 @@ Rails.application.routes.draw do
   
   get '/add_teammate_to_team/:id' => "teammates#add_teammate_to_team", :as => :add_teammate_to_team
  # get '/school_sort' => "students#school_sort", :as => :sort_school
-
+  
 
   get "/messages/multi_team_chat" => "messages#index_multi_team_chat", as: 'multi_team_chat'
   put "/messages/create_multi_team_chat" => "messages#create_multi_team_chat", as: 'create_multi_team_chat'
