0a69c77236ba8bc1128ff1ac86d41c93a4f321ef
Warren <warren.kushner@gmail.com>
2014-12-20 18:07:34 -0500

    got internal team chat working

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,15 @@ Rails.application.routes.draw do
   devise_for :users
 
 
+  get "/users/:id" => "users#binary_selection", as: 'binary_selection'
+  put "/users/:id" => "users#binary_selection_update", as: 'binary_selection_update'
+
   get "/users/admin_form" => "users#admin_form", as: 'admin_form'
   get "/users/:id" => "users#admin_show", as: 'admin_show'
   put "/users/:id" => "users#admin_info_create", as: 'admin_info_create'
 
 
 
-
-
-
-
-  get "/users/:id" => "users#binary_selection", as: 'binary_selection'
-  put "/users/:id" => "users#binary_selection_update", as: 'binary_selection_update'
 
 
   resources :users
