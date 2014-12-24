71c7743f1f8c0270d8d17991048bb001a582317e
Warren <warren.kushner@gmail.com>
2014-12-24 18:40:54 -0500

    added user-user chat, needs updated u-u

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -5,30 +5,39 @@ class ConversationsController < ApplicationController
     @teams = Team.all
     @conversation = Conversation.new
   end
+
   def create
     #creates conversation between two teams
     @conversation = Conversation.create!(message_params)
    ###########################
     # creates conversation between all teammates of two teams hold off for now
-    # @home_team = Team.find(home_team_param)
-    # @visiting_team = Team.find(visiting_team_param)
-    # @visiting_team.teammates.each do |visiting_teammate|
-    #   @home_team.teammates.each do |home_teammate|
-    #         Conversation.new
-    #         Conversation.create!(
-    #             :sender_id => current_user.id,
-    #             :recipient_id teammate.id
-    #           )
-    #           if @conversation.present?
-    #            # put something to verify a conversation was made or not
-    #           end
-    # end
+    @home_team = @conversation.home_team
+    @visiting_team = @conversation.visiting_team
+
+    @visiting_team.teammates.each do |visiting_teammate|
+      @home_team.teammates.each do |home_teammate|
+           @user_team_to_team =  Conversation.new
+           @user_team_to_team
+           @user_team_to_team = Conversation.create!(
+                :home_team_id => @conversation.home_team.id,
+                :visiting_team_id => @conversation.visiting_team.id,
+                :sender_id => visiting_teammate.id,
+                :recipient_id => home_teammate.id
+              )
+           @user_team_to_team
+              if @conversation.present?
+               # put something to verify a conversation was made or not
+              end
+        end
+    end
 
     if @conversation.present?
       redirect_to conversations_path
     end
   end
   def index
+
+
    @user_conversations = Conversation.where("team_id = ?", current_user.team.id).where("sender_id = ? OR recipient_id= ?", current_user.id, current_user.id)
     @conversations =  Conversation.where("home_team_id = ? OR visiting_team_id = ? OR team_id = ? ", current_user.team.id, current_user.team.id, current_user.team.id).where(:sender_id => nil).where(:recipient_id => nil)
       if current_user.team ==  Conversation.where("home_team_id = ? OR visiting_team_id = ? ", current_user.team.id, current_user.team.id).first.home_team
@@ -36,6 +45,8 @@ class ConversationsController < ApplicationController
       else
             @opposing_team = Conversation.where("home_team_id = ? OR visiting_team_id = ? ", current_user.team.id, current_user.team.id).first.home_team
       end
+            @opposing_team_conversations = Conversation.where("home_team_id = ? OR visiting_team_id = ? ", current_user.team.id, current_user.team.id)
+
   end
 
   protected
@@ -43,11 +54,11 @@ class ConversationsController < ApplicationController
     params.require(:conversation).permit(:home_team_id, :visiting_team_id)
   end
   ###############################
-  # code for individuals to chat within team to team chat hold off for now
-  # def home_team_param
-  #   params[:conversation][:home_team_id]
-  # end
-  # def visting_team_param
-  #   params[:conversation][:visiting_team_id]
-  # end
+  #code for individuals to chat within team to team chat hold off for now
+  def home_team_param
+    params[:conversation][:home_team_id]
+  end
+  def visting_team_param
+    params[:conversation][:visiting_team_id]
+  end
 end
