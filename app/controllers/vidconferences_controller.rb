83c88c8c78e446387cef655e8cfd40ef42fa7ec3
Warren Kushner <warren.kushner@gmail.com>
2015-02-25 17:51:12 -0500

    got tokbox to work

--- app/controllers/vidconferences_controller.rb
+++ app/controllers/vidconferences_controller.rb
@@ -1,32 +1,39 @@
 class VidconferencesController < ApplicationController
 
     def new
-      @new_session = Vidconference.new
+      @vidconference = Vidconference.new
 
     end
     def create
         # Replace with your OpenTok API key:
         api_key = ENV['OPENTOK_KEY']
         # Replace with your OpenTok API secret:
-        api_secret = ['OPENTOK_SECRET_KEY']
+        api_secret = ENV['OPENTOK_SECRET_KEY']
 
         opentok = OpenTok::OpenTok.new api_key, api_secret
         session = opentok.create_session
-        @new_session = Vidconference.create!(:session => session)
+        @new_session = Vidconference.create!(:session => session.to_s)
 
         @new_session.users << current_user
-        @new_session.users << User.find(params[:vidconference][:user_id])
+        @new_session.users << User.find(params[:vidconference][:users])
+        redirect_to @new_session
     end
 
     def show
-      @vidconference = current_user.vidconference.session
+     # binding.pry
+      vidconference = current_user.vidconference.session
        # Replace with your OpenTok API key:
+       @session = vidconference
         api_key = ENV['OPENTOK_KEY']
         # Replace with your OpenTok API secret:
-        api_secret = ['OPENTOK_SECRET_KEY']
-
+        api_secret = ENV['OPENTOK_SECRET_KEY']
+        #binding.pry
         opentok = OpenTok::OpenTok.new api_key, api_secret
-        @token = @vidconference.session_id.generate_token
+       # @videoconference = opentok.create_session.session_id
+        @token = opentok.generate_token(vidconference)
+
+        #        opentok.generate_token(@videoconference)
+
     end
 
 end
