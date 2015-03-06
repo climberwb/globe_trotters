1e80bd8051de039cbc0464c1c463f218c52c9c2b
Warren Kushner <warren.kushner@gmail.com>
2015-03-06 12:36:10 -0500

    [WIP] - validation in friendrequest. Figure out current_user in teams model

--- app/assets/javascripts/team_relationships.js
+++ app/assets/javascripts/team_relationships.js
@@ -8,21 +8,6 @@ var TeamRelationship = function (sender,receiver, cb) {
     "json");
 };
 
-
-//var ready;
-// ready = function() {
-
-//   $(document).on('click','#location-list li',function(event){
-//        $('#team_location').val($(this)[0].innerHTML);
-
-//        $(document).on('click','#save-address',function(event){
-//           $('#team_form').submit();
-//       });
-//     });
-// };
-//sender_team_relationship
-//receiver_team_relationship
-//##AddFriendTeam
 $(function() {
 
 
@@ -33,7 +18,7 @@ $(function() {
     $('#location-list').empty();
     TeamRelationship(sender_team,receiver_team, function(status) {
        document.getElementById('#AddFriendTeam').innerHTML = "Request Sent"
-      
+
     });
 
 
--- app/models/team_relationship.rb
+++ app/models/team_relationship.rb
@@ -2,4 +2,13 @@ class TeamRelationship < ActiveRecord::Base
   belongs_to :sender_team, class_name: "Team"
   belongs_to :receiver_team, class_name: "Team"
   has_many :teammate_relationships
+
+  #only one friend request can be sent at a time per team
+  validate :team_relations_count_within_limit, :on => :create
+  #binding.pry
+  def team_relations_count_within_limit
+    if self.where("sender_team_id = ?", current_user.team.id).count > 0
+      errors.add(:base, "Exceeded request limit")
+    end
+  end
 end
