62eacef076a086376611896bd76029aaf49f0bbb
Warren <warren.kushner@gmail.com>
2015-03-14 18:14:26 -0400

    team_relationships can be accepted and rejected

--- app/controllers/team_relationships_controller.rb
+++ app/controllers/team_relationships_controller.rb
@@ -26,15 +26,23 @@ class TeamRelationshipsController < ApplicationController
   def update
   end
   def team_accept
-    @team_relationship = TeamRelationship.where(params[:team_relationships][:sender_team_id]).where(params[:team_relationships][:receiver_team_id]).first
+    sender_id = params[:team_relationships][:sender_team_id].to_i
+    receiver_id = params[:team_relationships][:receiver_team_id].to_i
+    # @team_relationship = TeamRelationship.where(params[:team_relationships][:sender_team_id]).where(params[:team_relationships][:receiver_team_id]).first
+
+    @team_relationship = TeamRelationship.where(sender_team_id: sender_id, receiver_team_id: receiver_id).first
     @team_relationship_accept = @team_relationship.update_attributes(:accepted_at=> Time.new)
-    @team_relationship.save
+    @team_relationship.save!
     render :json => {:status => "Accept" }.to_json
    #
   end
 
   def team_decline
-    @team_relationship_accept = TeamRelationship.where(params[:team_relationships][:sender_team_id]).where(params[:team_relationships][:receiver_team_id]).first.update_attributes(:rejected_at=> Time.new)
+    sender_id = params[:team_relationships][:sender_team_id].to_i
+    receiver_id = params[:team_relationships][:receiver_team_id].to_i
+
+    @team_relationship = TeamRelationship.where(sender_team_id: sender_id, receiver_team_id: receiver_id).first
+    @team_relationship_accept = @team_relationship.update_attributes(:rejected_at=> Time.new)
     render :json => {:status => "Decline" }.to_json
   end
 
