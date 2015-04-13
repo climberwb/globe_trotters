1afe259727abc33f79e29d615caf4148e36fc049
Warren <warren.kushner@gmail.com>
2015-04-13 00:04:05 -0400

    created dependent destroy for answers and and icebreakers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ActiveRecord::Base
-  has_many :icebreaker_sessions
+  has_many :icebreaker_sessions, dependent: :destroy
   has_many :notebooks, :through => :icebreaker_sessions
 end
