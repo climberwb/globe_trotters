43d7c584a01cabe98fa7db9f36943dd4894c4e17
Warren <warren.kushner@gmail.com>
2015-04-14 19:53:17 -0400

    added comment in policy

--- app/policies/individual_relationship_policy.rb
+++ app/policies/individual_relationship_policy.rb
@@ -1,4 +1,7 @@
 class IndividualRelationshipPolicy < ApplicationPolicy
+#TODO go back to Using the Policy Rules to Restrict Controller Actions in bloc tutorial
+
+
   # def update?
   #   user.admin? or not record.published?
   # end
