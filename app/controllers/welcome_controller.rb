3a0de9f4c3b1c7e1875a6e64febf7a87910423ea
Warren <warren.kushner@gmail.com>
2015-05-04 09:48:16 -0400

    made flash notice to check email after sign in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,9 @@ class WelcomeController < ApplicationController
    # variables are meant to render globe only for welcome#index(root) page
    @controller="welcome"
    @action="index"
+  if current_user && current_user.sign_in_count == 0
+   flash[:notice] = "Please check your email to verify your account!"
+  end
 
   end
 
