85974e916bf7f15ac5fca0a3cf63a67f6451ab28
Warren <warren.kushner@gmail.com>
2015-05-07 10:16:02 -0400

    made changes to mail call

--- app/models/individual_relationship.rb
+++ app/models/individual_relationship.rb
@@ -14,6 +14,6 @@ class IndividualRelationship < ActiveRecord::Base
 
   def self.mail(sender)
     client = SendGrid::Client.new(api_user: ENV['SENDGRID_USERNAME'], api_key: ENV['SENDGRID_PASSWORD'])
-    client.send(SendGrid::Mail.new(to: sender.email, from: 'BeGoodGlobal', subject: 'You have a friend!', text: 'You have a new friend! Please sign in and video chat!', html: '<a href="#{base_url}">Meet your friend!</a>'))
+    client.send(SendGrid::Mail.new(to: sender.email, from: 'BeGoodGlobal', subject: 'You have a friend!', html: '<a href="hello.com">Hi there!</a>'))
   end
 end
