c681735b48054a53957176956ed1f7a084affbb1
Warren <warren.kushner@gmail.com>
2015-05-17 14:12:54 -0400

    added urls

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ if Rails.env.production?
     "Who in your life has helped you like the boy? What did they do?",
     "The girl in the video loves her father. Who do you love?",
     "The world is a big place, full of beauty and wonder. What is one place you would like to visit?, Why?",
-    "Which is your favorite Albert Einstein quote?, Why?",
-    "Which quote inspires you the most?, Why?"]
+    "what is your favorite breakfast meal?",
+    "what is your home like?"]
     # IMPORTANT! add s in front of https otherwise video will not rendor
     # IMPORTANT! for all youtube videos use embeded urls
 
@@ -25,8 +25,8 @@ if Rails.env.production?
       "https://www.youtube.com/embed/2x_Fl3NQVd4?list=PLdMisKontG3gHkeGlECyYfgt62bYjN5vN",
       "https://www.youtube.com/embed/Y2mk_zZCZVE?list=PLdMisKontG3gHkeGlECyYfgt62bYjN5vN",
       "https://www.youtube.com/embed/Ip2ZGND1I9Q?list=PLdMisKontG3gHkeGlECyYfgt62bYjN5vN",
-      "http://www.stumbleupon.com/su/3bA1d0/dVW9ndUi:49WCd1A4/parentpalace.com/2012/09/albert-einstein-quotes/",
-      "http://www.stumbleupon.com/su/6qad5B/e8zNJr2q:49WCd1A4/addicted2success.com/quotes/40-rare-motivational-inspirational-picture-quotes/"
+      "https://www.youtube.com/embed/ry1E1uzPSU0",
+      "https://youtu.be/mVoLH7DLqaI"
   ]
 
   questions.each_with_index do |question,index|
